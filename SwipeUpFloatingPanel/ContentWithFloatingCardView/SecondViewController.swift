cf89b15107dcf5b251f8d5a76337d9c4aa76c754
Giovanne Bressam <giovannebressam@gmail.com>
2022-01-31 00:26:40 -0300

    Moved all floating panel configuration to custom class

--- SwipeUpFloatingPanel/ContentWithFloatingCardView/SecondViewController.swift
+++ SwipeUpFloatingPanel/ContentWithFloatingCardView/SecondViewController.swift
@@ -8,157 +8,24 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
-    enum CardState {
-        case collapsed
-        case expanded
-    }
     
     var cardViewController: FloatingCardViewController!
-    var visualEffectView: UIVisualEffectView!
-    
-    var cardHeight: CGFloat = 300
-    let cardHandlerAreaHeight: CGFloat = 30
-    let defaultOriginY: CGFloat = 300
-    var isCardVisible: Bool = false
-    
-    var nextState: CardState {
-        return isCardVisible ? .collapsed : .expanded
-    }
-    
-    var runningAnimations = [UIViewPropertyAnimator]()
-    var animationProgressWhenInterrupted: CGFloat = 0
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        cardHeight = self.view.frame.height
-    }
-    
     
     func setupCardView() {
-        //Setup blur
-        visualEffectView = UIVisualEffectView()
-        visualEffectView.frame = self.view.bounds
-        visualEffectView.effect = UIBlurEffect(style: .dark)
-        visualEffectView.isHidden = true
-        self.view.addSubview(visualEffectView)
+        // content view
+        let contentView = ContentViewController()
         
         //Create card
-        cardViewController = FloatingCardViewController(nibName: "FloatingCardViewController", bundle: nil)
-        cardViewController.view.frame = CGRect(x: 0, y: self.view.bounds.height - (view.safeAreaInsets.bottom + self.cardHandlerAreaHeight + defaultOriginY), width: self.view.bounds.width, height: self.view.bounds.height)
+        cardViewController = FloatingCardViewController(containedController: contentView, cardHeight: self.view.frame.height)
+        cardViewController.view.frame = CGRect(x: 0, y: self.view.bounds.height - (view.safeAreaInsets.bottom + 30 + 300), width: self.view.bounds.width, height: self.view.bounds.height)
         self.addChild(cardViewController)
         self.view.addSubview(cardViewController.view)
+        cardViewController.didMove(toParent: self)
         cardViewController.view.clipsToBounds = true
         cardViewController.configureView(to: .moving)
-        
-        //Create gestures
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapCard(recognizer:)))
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanCard(recognizer:)))
-        self.cardViewController.cardHandleArea.addGestureRecognizer(tapGesture)
-        self.cardViewController.cardHandleArea.addGestureRecognizer(panGesture)
-        cardViewController.dismissButton.addTarget(self, action: #selector(dismissTapped), for: .touchUpInside)
-        
-        
-        let mainVC = ContentViewController(nibName: "ContentViewController", bundle: nil)
-        cardViewController.addChild(mainVC)
-        mainVC.view.frame = .init(x: 0, y: 0 + cardHandlerAreaHeight, width: cardViewController.view.frame.width, height: cardViewController.view.frame.height - cardHandlerAreaHeight)
-        cardViewController.view.addSubview(mainVC.view)
-        mainVC.didMove(toParent: cardViewController)
     }
     
-    //MARK: Card gesture handlers
-    @objc func dismissTapped() {
-        let lowerBound = view.frame.height - (view.safeAreaInsets.bottom + cardHandlerAreaHeight)
-        if let viewToDrag = cardViewController.view {
-            UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 2, options: .curveEaseOut) { [weak self] in
-                viewToDrag.frame.origin = .init(x: viewToDrag.frame.origin.x, y: lowerBound)
-                self?.visualEffectView.alpha = 0
-                self?.cardViewController.configureView(to: .moving)
-                self?.cardViewController.remove()
-            }
-        }
-    }
     
-    @objc func handleTapCard(recognizer: UITapGestureRecognizer) {
-        print("Tap")
-//        self.animateTransitionIfNeeded(nextState: self.nextState, duration: 0.9)
-    }
-    
-    @objc func handlePanCard(recognizer: UIPanGestureRecognizer) {
-        let translation = recognizer.translation(in: self.view)
-        
-        if let viewToDrag = cardViewController.view {
-            // Get the percentagem of current position related to current view's top
-            let percentage = (view.frame.height - viewToDrag.frame.origin.y)/view.frame.height
-            visualEffectView.isHidden = (percentage < 0.6)
-            
-            // Check if moving view is inside safearea to avoid going out of it
-            var newOriginY = viewToDrag.frame.origin.y + translation.y
-            let higherBound = view.safeAreaInsets.top
-            let lowerBound = view.frame.height - (view.safeAreaInsets.bottom + cardHandlerAreaHeight)
-            newOriginY = newOriginY < higherBound ? higherBound : newOriginY
-            newOriginY = newOriginY > lowerBound ? lowerBound : newOriginY
-            
-            switch recognizer.state {
-            case .changed:
-                // Move anywhere when changed
-                viewToDrag.frame.origin = .init(x: viewToDrag.frame.origin.x, y: newOriginY)
-                
-                // blur background
-                visualEffectView.alpha = percentage
-            case .ended:
-                // When ended, check if is over certain position and anchor it with animation
-                newOriginY = percentage > 0.6 ? higherBound : newOriginY
-                UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: .curveEaseIn) {
-                    viewToDrag.frame.origin = .init(x: viewToDrag.frame.origin.x, y: newOriginY)
-                } completion: { [weak self] _ in
-                    if percentage > 0.6 {
-                        self?.visualEffectView.isHidden = true
-                        self?.cardViewController.configureView(to: .fullScreen)
-                    }
-                }
-
-            case .cancelled:
-                fallthrough
-            default:
-                break
-            }
-            // Reset gesture speed
-            recognizer.setTranslation(.zero, in: viewToDrag)
-        }
-        
-//        if let viewToDrag = cardViewController.view {
-//            // Get the percentagem related to current view's top
-//            let percentage = (view.frame.height - viewToDrag.frame.origin.y)/view.frame.height
-//            visualEffectView.isHidden = (percentage < 0.4)
-//
-//            // Check if moving view is inside safearea
-//            var newOriginY = viewToDrag.frame.origin.y + translation.y
-//            let higherBound = view.safeAreaInsets.top
-//            let lowerBound = view.frame.height - (view.safeAreaInsets.bottom + cardHandlerAreaHeight)
-//            newOriginY = newOriginY < higherBound ? higherBound : newOriginY
-//            newOriginY = newOriginY > lowerBound ? lowerBound : newOriginY
-//
-//            // Check if new position fits next anchor
-//            let topAnchor: CGFloat = 200
-//            newOriginY = newOriginY < topAnchor ? higherBound : newOriginY
-//            viewToDrag.frame.origin = .init(x: viewToDrag.frame.origin.x, y: newOriginY)
-//            recognizer.setTranslation(.zero, in: viewToDrag)
-//
-//
-//            // blur background
-//            visualEffectView.alpha = percentage
-//        }
-    }
     
     @IBAction func addFloatingPanelTapped(_ sender: UIButton) {
         if cardViewController != nil {
--- SwipeUpFloatingPanel/FloatingCard/FloatingCardViewController.swift
+++ SwipeUpFloatingPanel/FloatingCard/FloatingCardViewController.swift
@@ -12,15 +12,56 @@ enum FloatingCardState {
 }
 
 class FloatingCardViewController: UIViewController {
+    
+    // MARK: Outlets
     @IBOutlet var cardHandleArea: UIView!
     @IBOutlet var horizontalLine: HorizontalLineRoundedCorners!
     @IBOutlet var dismissButton: UIButton!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // MARK: Properties
+    var containedController: UIViewController
+    var visualEffectView: UIVisualEffectView = .init(effect: UIBlurEffect(style: .dark))
 
-        // Do any additional setup after loading the view.
+    // layout configuration
+    var cardHeight: CGFloat = 300
+    let cardHandlerAreaHeight: CGFloat = 30
+    let defaultOriginY: CGFloat = 300
+    var isCardVisible: Bool = false
+    
+    init(containedController: UIViewController, cardHeight: CGFloat) {
+        self.containedController = containedController
+        self.cardHeight = cardHeight
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func didMove(toParent parent: UIViewController?) {
+        super.didMove(toParent: parent)
+        configureBlur()
+        configureContentView()
+        configureGestures()
+    }
+
+    func configureContentView() {
+        addChild(containedController)
+        containedController.view.frame = .init(x: 0, y: 0 + cardHandlerAreaHeight, width: view.frame.width, height: view.frame.height - cardHandlerAreaHeight)
+        view.addSubview(containedController.view)
+        containedController.didMove(toParent: self)
+    }
+    
+    func configureBlur() {
+        if let superView = parent?.view {
+            // Add blur under floating panel on parent
+            visualEffectView.frame = superView.bounds
+            visualEffectView.isHidden = true
+            superView.addSubview(visualEffectView)
+            
+            // Bring floating panel in front of blur
+            superView.bringSubviewToFront(self.view)
+        }
     }
     
     func configureView(to state: FloatingCardState) {
@@ -33,16 +74,100 @@ class FloatingCardViewController: UIViewController {
             dismissButton.isHidden = false
         }
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func configureGestures() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapCard(recognizer:)))
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanCard(recognizer:)))
+        cardHandleArea.addGestureRecognizer(tapGesture)
+        cardHandleArea.addGestureRecognizer(panGesture)
+        dismissButton.addTarget(self, action: #selector(dismissTapped), for: .touchUpInside)
     }
-    */
+    
+    //MARK: Card gesture handlers
+    @objc func dismissTapped() {
+        if let superView = parent?.view {
+            let lowerBound = superView.frame.height - (superView.safeAreaInsets.bottom + cardHandlerAreaHeight)
+            UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 2, options: .curveEaseOut) { [weak self] in
+                guard let self = self  else { return }
+                self.view.frame.origin = .init(x: self.view.frame.origin.x, y: lowerBound)
+                self.visualEffectView.alpha = 0
+                self.configureView(to: .moving)
+                self.remove()
+            }
+        }
+    }
+    
+    @objc func handleTapCard(recognizer: UITapGestureRecognizer) {
+        print("Tap")
+//        self.animateTransitionIfNeeded(nextState: self.nextState, duration: 0.9)
+    }
+    
+    @objc func handlePanCard(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        
+        if let superView = parent?.view {
+            // Get the percentagem of current position related to current view's top
+            let percentage = (superView.frame.height - view.frame.origin.y)/superView.frame.height
+            visualEffectView.isHidden = (percentage < 0.6)
 
+            // Check if moving view is inside safearea to avoid going out of it
+            var newOriginY = view.frame.origin.y + translation.y
+            let higherBound = superView.safeAreaInsets.top
+            let lowerBound = superView.frame.height - (superView.safeAreaInsets.bottom + cardHandlerAreaHeight)
+            newOriginY = newOriginY < higherBound ? higherBound : newOriginY
+            newOriginY = newOriginY > lowerBound ? lowerBound : newOriginY
+
+            switch recognizer.state {
+            case .changed:
+                // Move anywhere when changed
+                view.frame.origin = .init(x: view.frame.origin.x, y: newOriginY)
+
+                // blur background
+                visualEffectView.alpha = percentage
+            case .ended:
+                // When ended, check if is over certain position and anchor it with animation
+                newOriginY = percentage > 0.6 ? higherBound : newOriginY
+                UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: .curveEaseIn) { [weak self] in
+                    guard let self = self else { return }
+                    self.view.frame.origin = .init(x: self.view.frame.origin.x, y: newOriginY)
+                } completion: { [weak self] _ in
+                    guard let self = self else { return }
+                    if percentage > 0.6 {
+                        self.visualEffectView.isHidden = true
+                        self.configureView(to: .fullScreen)
+                    }
+                }
+
+            case .cancelled:
+                fallthrough
+            default:
+                break
+            }
+            // Reset gesture speed
+            recognizer.setTranslation(.zero, in: self.view)
+        }
+//
+//        if let viewToDrag = cardViewController.view {
+//            // Get the percentagem related to current view's top
+//            let percentage = (view.frame.height - viewToDrag.frame.origin.y)/view.frame.height
+//            visualEffectView.isHidden = (percentage < 0.4)
+//
+//            // Check if moving view is inside safearea
+//            var newOriginY = viewToDrag.frame.origin.y + translation.y
+//            let higherBound = view.safeAreaInsets.top
+//            let lowerBound = view.frame.height - (view.safeAreaInsets.bottom + cardHandlerAreaHeight)
+//            newOriginY = newOriginY < higherBound ? higherBound : newOriginY
+//            newOriginY = newOriginY > lowerBound ? lowerBound : newOriginY
+//
+//            // Check if new position fits next anchor
+//            let topAnchor: CGFloat = 200
+//            newOriginY = newOriginY < topAnchor ? higherBound : newOriginY
+//            viewToDrag.frame.origin = .init(x: viewToDrag.frame.origin.x, y: newOriginY)
+//            recognizer.setTranslation(.zero, in: viewToDrag)
+//
+//
+//            // blur background
+//            visualEffectView.alpha = percentage
+//        }
+    }
 }
